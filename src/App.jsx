6326f2c8ed64bda998c5f84909f44ca48acab5be
Mohammad Fadhli <marvelouslyfadhli@gmail.com>
2023-05-28 15:47:33 +0800

    merge

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import "./App.css";
 import { AuthProvider } from "./auth";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Login from "./components/Login";
-import NavBar from "./components/navbar";
+import NavBar from "./components/NavBar";
 import SignUp from "./components/Signup";
 import Home from "./pages/Home";
 
@@ -37,44 +37,4 @@ function App() {
   );
 }
 
-export default App;
-import "./App.css";
-import { AuthProvider } from "./auth";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Login from "./components/Login";
-import NavBar from "./components/NavBar";
-import SignUp from "./components/Signup";
-import AddPost from "./components/AddPost";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <NavBar></NavBar>,
-    children:[
-      {
-        path: "/login",
-        element: <Login></Login>
-      },
-      {
-        path: "/signup",
-        element: <SignUp></SignUp>
-      },
-      {
-        path: "/addpost",
-        element: <AddPost></AddPost>
-      },
-    ]
-  },
-])
-
-function App() {
-    return (
-        <>
-            <AuthProvider>
-                <RouterProvider router={router}></RouterProvider>
-            </AuthProvider>
-        </>
-    );
-}
-
-export default App;
+export default App
