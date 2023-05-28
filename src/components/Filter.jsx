f25d34765221237f7fad5277d29531d8334d14be
Goh Zhi Yi <gohzhiyi77@gmail.com>
2023-05-29 02:16:28 +0800

    filter buttons done

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -17,8 +17,12 @@ function Filter({setActiveCategory, activeCategory, setFiltered, posts}) {
         <div className="filter-container">
             <button onClick={() => setActiveCategory("default")}>All</button>
             <button onClick={() => setActiveCategory("Bakery")}>Bakery</button>
-            <button onClick={() => setActiveCategory("Fruits & Vegetables")}>Fruits & Vegetables</button>
+            <button onClick={() => setActiveCategory("Dairy, Chilled & Eggs")}>Dairy, Chilled & Eggs</button>
             <button onClick={() => setActiveCategory("Frozen")}>Frozen</button>
+            <button onClick={() => setActiveCategory("Fruits & Vegetables")}>Fruits & Vegetables</button>
+            <button onClick={() => setActiveCategory("Meat & Seafood")}>Meat & Seafood</button>
+            <button onClick={() => setActiveCategory("Rice, Noodles & Cooking Ingredients")}>Rice, Noodles & Cooking Ingredients</button>
+            <button onClick={() => setActiveCategory("Snacks & Confectionery")}>Snacks & Confectionery</button>
         </div>
     )
 }
