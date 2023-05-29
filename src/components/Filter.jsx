b2b1753cad32fba03febd4c5c2d5510606e2d6aa
Fadhli <marvelouslyfadhli@gmail.com>
2023-05-30 04:28:09 +0800

    fixed filter btns 2

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -15,14 +15,14 @@ function Filter({setActiveCategory, activeCategory, setFiltered, posts}) {
 
     return (
         <div className="filter-container">
-            <button class={activeCategory === "default" ? "btn active" : "btn"} onClick={() => setActiveCategory("default")}>All</button>
-            <button class={activeCategory === "Bakery" ? "btn active" : "btn"} onClick={() => setActiveCategory("Bakery")}>Bakery</button>
-            <button class={activeCategory === "Dairy, Chilled & Eggs" ? "btn active" : "btn"} onClick={() => setActiveCategory("Dairy, Chilled & Eggs")}>Dairy, Chilled & Eggs</button>
-            <button class={activeCategory === "Frozen" ? "btn active" : "btn"} onClick={() => setActiveCategory("Frozen")}>Frozen</button>
-            <button class={activeCategory === "Fruits & Vegetables" ? "btn active" : "btn"} onClick={() => setActiveCategory("Fruits & Vegetables")}>Fruits & Vegetables</button>
-            <button class={activeCategory === "Meat & Seafood" ? "btn active" : "btn"} onClick={() => setActiveCategory("Meat & Seafood")}>Meat & Seafood</button>
-            <button class={activeCategory === "Rice, Noodles & Cooking Ingredients" ? "btn active" : "btn"} onClick={() => setActiveCategory("Rice, Noodles & Cooking Ingredients")}>Rice, Noodles & Cooking Ingredients</button>
-            <button class={activeCategory === "Snacks & Confectionery" ? "btn active" : "btn"} onClick={() => setActiveCategory("Snacks & Confectionery")}>Snacks & Confectionery</button>
+            <button class={activeCategory === "default" ? "btn active filterbtn" : "btn filterbtn"} onClick={() => setActiveCategory("default")}>All</button>
+            <button class={activeCategory === "Bakery" ? "btn active filterbtn" : "btn filterbtn"} onClick={() => setActiveCategory("Bakery")}>Bakery</button>
+            <button class={activeCategory === "Dairy, Chilled & Eggs" ? "btn active filterbtn" : "btn filterbtn"} onClick={() => setActiveCategory("Dairy, Chilled & Eggs")}>Dairy, Chilled & Eggs</button>
+            <button class={activeCategory === "Frozen" ? "btn active filterbtn" : "btn filterbtn"} onClick={() => setActiveCategory("Frozen")}>Frozen</button>
+            <button class={activeCategory === "Fruits & Vegetables" ? "btn active filterbtn" : "btn filterbtn"} onClick={() => setActiveCategory("Fruits & Vegetables")}>Fruits & Vegetables</button>
+            <button class={activeCategory === "Meat & Seafood" ? "btn active filterbtn" : "btn"} onClick={() => setActiveCategory("Meat & Seafood")}>Meat & Seafood</button>
+            <button class={activeCategory === "Rice, Noodles & Cooking Ingredients" ? "btn active filterbtn" : "btn filterbtn"} onClick={() => setActiveCategory("Rice, Noodles & Cooking Ingredients")}>Rice, Noodles & Cooking Ingredients</button>
+            <button class={activeCategory === "Snacks & Confectionery" ? "btn active filterbtn" : "btn filterbtn"} onClick={() => setActiveCategory("Snacks & Confectionery")}>Snacks & Confectionery</button>
         </div>
     )
 }
